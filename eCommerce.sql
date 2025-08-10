1f23556fb9ef2c9880a3b35d93adf2e5f11edb74
Clara <claraa.mino12@gmail.com>
2025-08-10 10:49:15 -0300

    nueva db

--- eCommerce.sql
+++ eCommerce.sql
@@ -1,122 +1,9 @@
--- -- phpMyAdmin SQL Dump
--- -- version 5.2.2
--- -- https://www.phpmyadmin.net/
--- --
--- -- Servidor: mysql:3306
--- -- Tiempo de generación: 06-08-2025 a las 03:41:38
--- -- Versión del servidor: 5.7.44
--- -- Versión de PHP: 8.2.28
-
--- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
--- START TRANSACTION;
--- SET time_zone = "+00:00";
-
-
--- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
--- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
--- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
--- /*!40101 SET NAMES utf8mb4 */;
-
--- --
--- -- Base de datos: `eCommerce`
--- --
-
--- -- --------------------------------------------------------
-
--- --
--- -- Estructura de tabla para la tabla `Category`
--- --
-
--- CREATE TABLE `Category` (
---   `id` int(11) NOT NULL,
---   `name` varchar(255) NOT NULL,
---   `description` text NOT NULL,
---   `creation_date` datetime DEFAULT CURRENT_TIMESTAMP,
---   `deleted` tinyint(1) NOT NULL DEFAULT '0'
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --
--- -- Volcado de datos para la tabla `Category`
--- --
-
--- INSERT INTO `Category` (`id`, `name`, `description`, `creation_date`, `deleted`) VALUES
--- (1, 'pantalonesEDITADO', 'pantalonesEDITADO', '2025-07-28 20:21:19', 0),
--- (2, 'remeras', 'prenda de ropa remeras', '2025-07-28 20:21:19', 0),
--- (5, 'ejemplo', 'ejemplo', '2025-07-31 15:15:47', 0);
-
--- -- --------------------------------------------------------
-
--- --
--- -- Estructura de tabla para la tabla `Product`
--- --
-
--- CREATE TABLE `Product` (
---   `id` int(11) NOT NULL,
---   `name` varchar(255) NOT NULL,
---   `description` text NOT NULL,
---   `price` decimal(10,0) NOT NULL,
---   `stock` int(11) NOT NULL DEFAULT '0',
---   `state` enum('active','inactive','out_of_stock','hidden') NOT NULL DEFAULT 'active',
---   `imageUrl` varchar(255) NOT NULL,
---   `creation_date` datetime DEFAULT CURRENT_TIMESTAMP,
---   `categoryId` int(11) NOT NULL,
---   `deleted` tinyint(1) NOT NULL DEFAULT '0'
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --
--- -- Volcado de datos para la tabla `Product`
--- --
-
--- INSERT INTO `Product` (`id`, `name`, `description`, `price`, `stock`, `state`, `imageUrl`, `creation_date`, `categoryId`, `deleted`) VALUES
--- (1, 'remera levis L', 'remera talle L over', 55000, 7, 'active', '', '2025-07-28 20:34:30', 2, 0),
--- (2, 'remera2', 'remera xl', 52777, 3, 'active', '', '2025-07-28 20:39:08', 2, 0),
--- (3, 'remera boxy fit', 'no se', 88000, 8, 'active', '', '2025-07-28 20:39:08', 2, 0),
--- (4, 'pantapalon1', 'alto lompa', 50000, 20, 'active', '', '2025-07-28 20:40:00', 1, 0),
--- (5, 'lompa2', 'otro lompa', 40000, 2, 'active', '', '2025-07-28 20:40:22', 1, 0);
-
--- --
--- -- Índices para tablas volcadas
--- --
-
--- --
--- -- Indices de la tabla `Category`
--- --
--- ALTER TABLE `Category`
---   ADD PRIMARY KEY (`id`);
-
--- --
--- -- Indices de la tabla `Product`
--- --
--- ALTER TABLE `Product`
---   ADD PRIMARY KEY (`id`),
---   ADD KEY `id_category` (`categoryId`) USING BTREE;
-
--- --
--- -- AUTO_INCREMENT de las tablas volcadas
--- --
-
--- --
--- -- AUTO_INCREMENT de la tabla `Category`
--- --
--- ALTER TABLE `Category`
---   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
--- --
--- -- AUTO_INCREMENT de la tabla `Product`
--- --
--- ALTER TABLE `Product`
---   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
--- COMMIT;
-
--- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 -- phpMyAdmin SQL Dump
 -- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: mysql:3306
--- Tiempo de generación: 09-08-2025 a las 16:57:42
+-- Tiempo de generación: 10-08-2025 a las 13:47:55
 -- Versión del servidor: 5.7.44
 -- Versión de PHP: 8.2.29
 
@@ -144,8 +31,17 @@ CREATE TABLE `Category` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Category`
+--
+
+INSERT INTO `Category` (`id`, `name`, `created_at`, `updated_at`, `deleted`) VALUES
+(1, 'Remeras', '2025-08-09 19:13:24', '2025-08-09 19:13:24', 0),
+(2, 'Pantalones', '2025-08-09 19:13:36', '2025-08-09 19:13:36', 0);
 
 -- --------------------------------------------------------
 
@@ -189,16 +85,19 @@ CREATE TABLE `Product` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `color` varchar(255) NOT NULL,
-  `size` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `stock` int(11) NOT NULL DEFAULT '0',
-  `state` enum('active','inactive','out_of_stock','hidden') NOT NULL DEFAULT 'active',
   `categoryId` int(11) NOT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Product`
+--
+
+INSERT INTO `Product` (`id`, `name`, `description`, `price`, `categoryId`, `deleted`, `created_at`, `updated_at`) VALUES
+(2, 'Remera Levis', 'Remera overzize', 25000.00, 1, 0, '2025-08-09 19:29:17', '2025-08-09 19:29:17');
 
 -- --------------------------------------------------------
 
@@ -213,6 +112,39 @@ CREATE TABLE `Product_Images` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Product_Images`
+--
+
+INSERT INTO `Product_Images` (`id`, `product_id`, `image_url`, `created_at`, `updated_at`) VALUES
+(2, 2, 'https://tse4.mm.bing.net/th/id/OIP.YyDGMLI27R_TQ47sOk5KEwHaLH?r=0&rs=1&pid=ImgDetMain&o=7&rm=3', '2025-08-09 19:32:00', '2025-08-09 19:32:00'),
+(3, 2, 'https://tse4.mm.bing.net/th/id/OIP.g8RvSZ8aAYNV1ReswJbSvAAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3', '2025-08-09 19:32:44', '2025-08-09 19:32:44');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Product_Variants`
+--
+
+CREATE TABLE `Product_Variants` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `color` varchar(50) NOT NULL,
+  `size` varchar(10) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `state` enum('active','inactive','out_of_stock','hidden') NOT NULL DEFAULT 'active',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Product_Variants`
+--
+
+INSERT INTO `Product_Variants` (`id`, `product_id`, `color`, `size`, `stock`, `state`, `deleted`) VALUES
+(1, 2, 'Rojo', 'L', 10, 'active', 0),
+(2, 2, 'Rojo', 'XL', 15, 'active', 0),
+(3, 2, 'Negro', 'S', 12, 'active', 0);
 
 -- --------------------------------------------------------
 
@@ -270,6 +202,13 @@ ALTER TABLE `Product_Images`
   ADD KEY `product_id` (`product_id`);
 
 --
+-- Indices de la tabla `Product_Variants`
+--
+ALTER TABLE `Product_Variants`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
 -- Indices de la tabla `User`
 --
 ALTER TABLE `User`
@@ -284,7 +223,7 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT de la tabla `Category`
 --
 ALTER TABLE `Category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `Orders`
@@ -302,13 +241,19 @@ ALTER TABLE `Order_Detail`
 -- AUTO_INCREMENT de la tabla `Product`
 --
 ALTER TABLE `Product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `Product_Images`
 --
 ALTER TABLE `Product_Images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `Product_Variants`
+--
+ALTER TABLE `Product_Variants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `User`
@@ -338,6 +283,12 @@ ALTER TABLE `Product`
 --
 ALTER TABLE `Product_Images`
   ADD CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `Product_Variants`
+--
+ALTER TABLE `Product_Variants`
+  ADD CONSTRAINT `product_variants_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
